7f159df1fe526e9f183a1b32178a24c4210e3f1b
MrR3set <bairon.pazrobles@gmail.com>
2020-05-05 13:09:58 -0700

    added user_days table

---
+++ data/migrations/20200505130419_add_Users_Days_Table.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("User_Days",User_Days=>{
+        User_Days.integer("Goal_ID")
+          .primary()
+          .notNullable()
+          .references("ID")
+          .inTable("Goals")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+        User_Days.date("Date");
+        User_Days.boolean("Success").defaultsTo(false);
+        User_Days.integer("DailyCount",4);
+      })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("User_Days")
+};
