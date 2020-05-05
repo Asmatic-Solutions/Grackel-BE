50a0f23f31ebff8d333b9a8a132a25f9c8d975fa
MrR3set <bairon.pazrobles@gmail.com>
2020-05-05 13:12:39 -0700

    added products table

--- data/migrations/20200505130919_add_Products_Table.js
+++ data/migrations/20200505130919_add_Products_Table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("Products",Products=>{
+        Products.integer("ID");
+        Products.string("Name",50).notNullable().index();
+        Products.string("Category",32).notNullable();
+        Products.integer("KcalPerG",4);
+      })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("Products");
 };
