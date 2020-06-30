aceecd70d6a14c35bd351fd1326a02b930edb68c
MrR3set <bairon.pazrobles@gmail.com>
2020-06-30 12:41:52 -0700

    Changed Role FK to Role_ID

--- data/migrations/20200425215348_add_users_table.js
+++ data/migrations/20200425215348_add_users_table.js
@@ -2,18 +2,18 @@
 exports.up = function(knex) {
   return knex.schema.createTable("Users",Users=>{
     Users.increments("ID").index();
-    Users.string("Username",50).notNullable().index();
+    Users.string("Username",50).notNullable().index().unique();
     Users.string("Password",255).notNullable();
     Users.string("Email").notNullable();
     Users.datetime("Created_at");
     Users.datetime("Lastconnection_at");
-    Users.integer("Role",1)
+    Users.integer("Role_ID",1)
       .notNullable()
       .references("ID")
       .inTable("Roles")
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
-    //Rol foreing key
+    //Role foreing key
   })
 
   
