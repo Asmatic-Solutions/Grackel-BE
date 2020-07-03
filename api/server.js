d7950e1ce87a62ace12817ef847f729c3a6c0730
MrR3set <bairon.pazrobles@gmail.com>
2020-07-03 13:36:23 -0700

    Added goal route

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors")
 
+const authenticator = require("./auth/authenticator");
 const authRouter = require("./auth/authRouter");
+const goalRouter = require("./goals/goalsRouter");
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
 server.use("/api/auth",authRouter);
+
+server.use("/api/goal", authenticator, goalRouter);
 
 
 server.get("/", (req,res)=>{
