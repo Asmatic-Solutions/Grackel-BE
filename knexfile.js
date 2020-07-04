2fb19dd2589160c3e816056574a9a76e4864907d
MrR3set <bairon.pazrobles@gmail.com>
2020-07-03 23:02:28 -0700

    added Enviroment variable for the database URI on production

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations/'
     },
@@ -15,11 +15,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
