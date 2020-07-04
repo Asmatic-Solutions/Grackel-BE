38981ea0fb07137bdfd1642c8923066f56527bc2
MrR3set <bairon.pazrobles@gmail.com>
2020-07-03 22:11:07 -0700

    Removed console log

--- api/goals/goalsRouter.js
+++ api/goals/goalsRouter.js
@@ -1,6 +1,48 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const secret = require("../auth/secrets").secret;
+const db = require("../../data/db-config");
+const Goals = require("./goalsModel");
 
 //Create new goal
+router.post("/",goalVerification,(req,res)=>{ 
+    getIDbyusername(req.headers.authorization).then(({ID})=>{  // Retrive ID by the username provided in the token 
+        Goals.getGoal(ID).then(data=>{ //Check if the ID already has a goal set up
+            if(data)
+                res.status(200).json({Message:"Goals has already been set"})
+            else
+                Goals.createGoal(req.body.goal,ID).then(goal=>{ // Pass the ID and the goal
+                    res.status(201).json({message:"Goal created succesfully",goal})
+                }).catch(err=>{
+                    console.log(err);
+                    res.status(500).json({message:"Error creating goal"})
+                })
+        }) 
+    })
+})
+
+//Delete goal
+router.post("/",(req,res)=>{
+    if(!(req.body.goal_ID)){
+        return res.status(403).json({Message:"Please provide goal_ID"});
+    }
+
+
+    //Check if goals exists
+
+    //Delete goal duh
+
+})
+
+
+
+router.get("/",goalVerification,(req,res)=>{
+    res.status(201).json("ok")
+})
+
+router.put("/",goalVerification,(req,res)=>{
+    res.status(201).json("ok")
+})
 
 //Get Goal
 
@@ -13,11 +55,24 @@ const router = require("express").Router();
 
 //TEST ROUTE
 router.get("/aa",(req,res)=>{
-    console.log(secret);
-    Users.getUserByID("8").then(data=>{
-        console.log("Test data",data)
-    })
+    Goals.getGoal(1).then(goal=>{
+        console.log("goal",goal)
+    });
 })
 
 module.exports = router;
+
+function goalVerification(req,res,next){
+    if(!(req.body.goal)){
+        return res.status(403).json({Message:"Please provide goal"});
+    }
+    next();
+}
+
+function getIDbyusername(token){
+    const {username} = jwt.verify(token,secret);
+    return db("Users").select("ID").where({"Username":username}).first().then((id)=>{
+        return id;
+    })
+}
 
