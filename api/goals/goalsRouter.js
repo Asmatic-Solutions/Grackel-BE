76915b7bc1aa0c5b941f9d726f9a229c09fa024a
MrR3set <bairon.pazrobles@gmail.com>
2020-07-05 00:20:17 -0700

    Fixed get to work with new DB schema

--- api/goals/goalsRouter.js
+++ api/goals/goalsRouter.js
@@ -61,7 +61,7 @@ router.put("/",goalValidation,(req,res)=>{
 router.get("/",(req,res)=>{
     getIDbyusername(req.headers.authorization).then(({ID})=>{  // Retrive ID by the username provided in the token 
         Goals.getGoal(ID).then(data=>{ //Check if the ID already has a goal set up
-            if(data)    
+            if(data==null)    
                 res.status(200).json(data);
             else
                 res.status(200).json({Message:"No goal data was found linked with the specified ID"})
