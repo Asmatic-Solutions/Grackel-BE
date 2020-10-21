29da7a04850c9fd98470cb775eebe2db3e5044b9
MrR3set <bairon.pazrobles@gmail.com>
2020-10-20 22:07:48 -0700

    Added JSON column name

---
+++ data/migrations/20201020220128_add_Procuts_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("Products", Products => {
+    Products.integer("ID").index();
+    Products.string("Name",50).notNullable().index();
+    Products.string("Category",50).notNullable();
+    Products.json("Nutrition_facts").notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("Product")
+};
