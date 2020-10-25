686b9f54bdad8474990e9e3aa23383eca395737b
MrR3set <bairon.pazrobles@gmail.com>
2020-10-24 21:57:35 -0700

    Added meal validation function

--- api/Meals/MealsRouter.js
+++ api/Meals/MealsRouter.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+
+router.post("/",mealValidation,(req,res)=>{
+    // Add current date to the obj
+    // Link with user ID
 
 
-router.post("/",(req,res)=>{
-    return res.status(201).json("MEALS WORKS GOOD")
     // Create meal based on req
     // Check if manual mode is active
         // Ignore the verification of Meals_Products [DB products]
@@ -10,6 +13,8 @@ router.post("/",(req,res)=>{
     // Else [STRETCH]
         // Write current meal ID on Product_Meals
         // Write custom products 
+
+    return res.status(201).json("MEALS WORKS GOOD")
 })
 
 router.get("/",(req,res)=>{
@@ -24,6 +29,29 @@ router.get("/",(req,res)=>{
 
 module.exports = router;
 
+function mealValidation(req,res,next){
+    if(!(req.body.type))
+        return res.status(403).json({message:"Missing type of meal"})
+    
+        if(req.body.manual){ // Verify manual meal mode
+            if (!(req.body.manual_Ingredients) || req.body.manual_Ingredients.length<=0) // Check for manual_Ingredients array
+                return res.status(403).json({message:"Missing manual ingredients array or array is empty"})
 
+            const {manual_Ingredients} = req.body
+            // Check for good format on manual ingredients
+            manual_Ingredients.forEach(ingredient => {
+                if (!(ingredient.name))
+                    return res.status(403).json({message:"Manual ingredients must have a name"})
+            });  
+        }
 
+    next();
+}
+
+function getIDbyusername(token){
+    const {username} = jwt.verify(token,secret);
+    return db("Users").select("ID").where({"Username":username}).first().then((id)=>{
+        return id;
+    })
+}
 
