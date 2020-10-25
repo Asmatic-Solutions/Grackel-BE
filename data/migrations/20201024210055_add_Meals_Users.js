c62621fb0cdf4a21187b9493afda40ae3e85253f
MrR3set <bairon.pazrobles@gmail.com>
2020-10-24 22:24:41 -0700

    Fixed typos and erros

--- data/migrations/20201024210055_add_Meals_Users.js
+++ data/migrations/20201024210055_add_Meals_Users.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
-    return knex.schema.createTable("Meals_Users", Products => {
-      Products.integer("ID").index();
-      Products.string("Type").notNullable().index();
-      Products.date("Date").notNullable();
-      Products.jsonb("Manual_Products")
-      Products.boolean("Manual").notNullable(); //Wether it is fully manual or not
-      Products.integer("User_ID")
+    return knex.schema.createTable("Meals_Users", Meals_Users => {
+      Meals_Users.increments("ID").index();
+      Meals_Users.string("Type").notNullable().index();
+      Meals_Users.date("Date").notNullable();
+      Meals_Users.jsonb("Manual_Ingredients")
+      Meals_Users.boolean("Manual").notNullable(); //Wether it is fully manual or not
+      Meals_Users.integer("User_ID")
       .notNullable()
       .references("ID")
       .inTable("Users")
