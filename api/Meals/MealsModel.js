c9e03f21ca846696c9515962c53ad714ba6dee2d
MrR3set <bairon.pazrobles@gmail.com>
2020-10-24 22:34:51 -0700

    Created manual meal add function and get last meal by user

---
+++ api/Meals/MealsModel.js
@@ -0,0 +1,35 @@
+const db = require("../../data/db-config");
+module.exports = {
+    createManualMeal
+}
+
+function createManualMeal(meal){
+    const date = new Date().toISOString();
+    meal = {Date:date,...meal,Manual_Ingredients:JSON.stringify(convertManualIngredients(meal.Manual_Ingredients))}
+    return db("Meals_Users").insert(meal).then(()=>{
+        return getLastMeal(meal.User_ID)
+    })
+}
+
+function getLastMeal(User_ID){
+    return db("Meals_Users").orderBy("Date","desc").select("*").where({User_ID}).limit(1).first().then(data=>{
+        // Check if its manual mode, if so simply return data
+        return data
+    })
+}
+
+
+
+
+function convertManualIngredients(Ingredients){
+
+    return Ingredients.map(Ingredients=>{
+      return {
+          name:Ingredients.name,
+          category:Ingredients.category,
+          calories:Ingredients.calories,
+          notes:Ingredients.notes
+      }
+    })
+
+  }
