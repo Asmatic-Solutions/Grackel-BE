75960cebc2cb6c017d075ce1065e7bb7c06b5e5b
MrR3set <bairon.pazrobles@gmail.com>
2020-11-15 16:37:47 -0800

    Changed seed to naming convention

--- data/seeds/01_Roles.js
+++ data/seeds/01_Roles.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('Roles').del()
+  return knex('roles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('Roles').insert([
-        {ID: 1, Role: 'Admin'},
-        {ID: 2, Role: 'Standard User'},
+      return knex('roles').insert([
+        {id: 1, role: 'Admin'},
+        {id: 2, role: 'Standard User'},
       ]);
     });
 };
--- data/seeds/02_TestingUsers.js
+++ data/seeds/02_TestingUsers.js
@@ -1,37 +1,37 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('Users').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
       const date = new Date().toISOString();
       return knex('Users').insert([
         {
-        "ID":1,
-        "Username":"MrR3set",
-        "Password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
-        "Email":"n@n.com",
-        "Created_at":date,
-        "Lastconnection_at":date,
-        "Role_ID":2
+        "id":1,
+        "username":"MrR3set",
+        "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
+        "email":"n@n.com",
+        "created_at":date,
+        "lastconnection_at":date,
+        "role_id":2
         },
         {
-        "ID":2,
-        "Username":"Sarah",
-        "Password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
-        "Email":"n@n.com",
-        "Created_at":date,
-        "Lastconnection_at":date,
-        "Role_ID":1
+        "id":2,
+        "isername":"Sarah",
+        "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
+        "email":"n@n.com",
+        "created_at":date,
+        "lastconnection_at":date,
+        "role_id":1
         },
         {
-        "ID":3,
-        "Username":"Joseph",
-        "Password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
-        "Email":"n@n.com",
-        "Created_at":date,
-        "Lastconnection_at":date,
-        "Role_ID":2
+        "id":3,
+        "isername":"Joseph",
+        "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
+        "email":"n@n.com",
+        "created_at":date,
+        "lastconnection_at":date,
+        "role_id":2
         },
       ]);
     });
