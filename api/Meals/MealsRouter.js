d8d0db7c5429fef724019de8aaa61dd2a5a958d0
MrR3set <bairon.pazrobles@gmail.com>
2020-11-15 18:00:42 -0800

    Fixed name convention

--- api/Meals/MealsRouter.js
+++ api/Meals/MealsRouter.js
@@ -6,9 +6,9 @@ const Meals = require("./MealsModel");
 
 router.post("/",mealValidation,(req,res)=>{
     let meal = req.body;
-    getIDbyusername(req.headers.authorization).then(({ID})=>{
-        meal = {User_ID:ID,...meal}
-        if(meal.Manual) // Check if manual mode is active
+    getIDbyusername(req.headers.authorization).then(({id})=>{
+        meal = {user_id:id,...meal}
+        if(meal.manual) // Check if manual mode is active
             // Ignore the verification of Meals_Products [DB products] and simply add the meals
             Meals.createManualMeal(meal).then(data=>{
                 if(data)
@@ -25,20 +25,22 @@ router.post("/",mealValidation,(req,res)=>{
 
 router.get("/",(req,res)=>{
     const {time,timeperiod} = req.query
-    getIDbyusername(req.headers.authorization).then(({ID})=>{
-            Meals.getLastMealsFrom(ID,time,timeperiod).then(data=>{
-                res.status(200).json(data)
-            }).catch(err=>{
-                console.log(err)
-                res.status(500).json(err)
-            })
+    getIDbyusername(req.headers.authorization).then(({id})=>{
+        Meals.getLastMealsFrom(id,time,timeperiod).then((data)=>{
+            data
+            ?res.status(200).json(data)
+            :res.status(404).json({message:"No meals found"})
+        }).catch(err=>{
+            console.log(err)
+            res.status(500).json(err)
+        })
     })
 })
 
 router.get("/on",(req,res)=>{
     const {date} = req.query
-    getIDbyusername(req.headers.authorization).then(({ID})=>{
-            Meals.getMealOn(ID,date).then(data=>{
+    getIDbyusername(req.headers.authorization).then(({id})=>{
+            Meals.getMealOn(id,date).then(data=>{
                 res.status(200).json(data)
             }).catch(err=>{
                 console.log(err)
@@ -50,16 +52,16 @@ router.get("/on",(req,res)=>{
 module.exports = router;
 
 function mealValidation(req,res,next){
-    if(!(req.body.Type))
+    if(!(req.body.type))
         return res.status(403).json({message:"Missing type of meal"})
     
-        if(req.body.Manual){ // Verify manual meal mode
-            if (!(req.body.Manual_Ingredients) || req.body.Manual_Ingredients.length<=0) // Check for manual_Ingredients array
+        if(req.body.manual){ // Verify manual meal mode
+            if (!(req.body.manual_ingredients) || req.body.manual_ingredients.length<=0) // Check for manual_Ingredients array
                 return res.status(403).json({message:"Missing manual ingredients array or array is empty"})
 
-            const {Manual_Ingredients} = req.body
+            const {manual_ingredients} = req.body
             // Check for good format on manual ingredients
-            Manual_Ingredients.forEach(ingredient => {
+            manual_ingredients.forEach(ingredient => {
                 if (!(ingredient.name))
                     return res.status(403).json({message:"Manual ingredients must have a name"})
             });  
@@ -70,7 +72,7 @@ function mealValidation(req,res,next){
 
 function getIDbyusername(token){
     const {username} = jwt.verify(token,secret);
-    return db("Users").select("ID").where({"Username":username}).first().then((id)=>{
+    return db("users").select("id").where({username}).first().then((id)=>{
         return id;
     })
 }
