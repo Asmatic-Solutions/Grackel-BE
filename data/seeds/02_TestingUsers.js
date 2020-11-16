824f8d41b4c03a6e01a52f855c5a23f47d428827
MrR3set <bairon.pazrobles@gmail.com>
2020-11-15 17:08:55 -0800

    Fixed truncate on user ID

--- data/seeds/02_TestingUsers.js
+++ data/seeds/02_TestingUsers.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex.raw('TRUNCATE users CASCADE')
     .then(function () {
       // Inserts seed entries
       const date = new Date().toISOString();
       return knex('users').insert([
         {
-        "id":1,
         "username":"MrR3set",
         "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
         "email":"n@n.com",
@@ -16,7 +15,6 @@ exports.seed = function(knex) {
         "role_id":2
         },
         {
-        "id":2,
         "username":"Sarah",
         "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
         "email":"n@n.com",
@@ -25,7 +23,6 @@ exports.seed = function(knex) {
         "role_id":1
         },
         {
-        "id":3,
         "username":"Joseph",
         "password":"$2b$08$.3a26tb525suKvOTrAvup.sLEkHer6s7S5Y1TZkJ0h7L1ofz7Q9ii",
         "email":"n@n.com",
